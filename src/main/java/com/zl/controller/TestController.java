0bd3df23e9632e1439bc91beffe4269a614d05a8
albertzh <albertzh@wicresoft.com>
2019-09-03 16:07:21 +0800

    update

--- src/main/java/com/zl/controller/TestController.java
+++ src/main/java/com/zl/controller/TestController.java
@@ -6,10 +6,24 @@ import com.zl.excel.ExcelExportUtil;
 import com.zl.excel.ExportParams;
 import com.zl.excel.style.ExcelExportStylerCustomImpl;
 import com.zl.util.ConcurrentDateUtil;
+import com.zl.util.PoiMergeCellUtil;
+import com.zl.util.PublicUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,11 +34,12 @@ import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
-@RestController
+@Controller
 public class TestController {
 
 
     @GetMapping(value = "/test")
+    @ResponseBody
     public String test(HttpServletResponse response, HttpServletRequest request) {
 
         List<User> list = new ArrayList<>();
@@ -46,6 +61,38 @@ public class TestController {
         ExcelUtils.exportExcel(workbook, "名单列表.xlsx", response, request);
         return "SUCCESS";
         //maiqun123a
+    }
+
+    @GetMapping(value = "/show")
+    public void show(HttpServletResponse response, HttpServletRequest request) {
+        System.out.println("show");
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet1 = workbook.createSheet("整体概况");
+        Row row = sheet1.createRow(0);
+        XSSFRichTextString xssfRichTextString = new XSSFRichTextString("58(行销推广部) 得分概览\n排名为前台分组中的排名，前台分组包括：行销推广部");
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setWrapText(true);
+//        cell.setCellStyle(cellStyle);
+        for (int i = 0; i < 6; i++) {
+            Cell cell1 = row.createCell(i);
+            cell1.setCellStyle(cellStyle);
+        }
+        Cell cell = row.getCell(0);
+        cell.setCellValue("58(行销推广部) 得分概览\n排名为前台分组中的排名，前台分组包括：行销推广部");
+        row.setHeightInPoints(2 * sheet1.getDefaultRowHeightInPoints());
+        PoiMergeCellUtil.addMergedRegion(sheet1, 0, 0, 0, 5);
+        ExcelUtils.exportExcel(workbook, "鼠标列表.xlsx", response, request);
+    }
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "show";
     }
 
 }
--- src/main/java/com/zl/excel/ExcelExportUtil.java
+++ src/main/java/com/zl/excel/ExcelExportUtil.java
@@ -38,4 +38,6 @@ public class ExcelExportUtil {
         return workbook;
     }
 
+
+
 }
